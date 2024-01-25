f5522aed86f5d3e5f4ff78eddf02046f3d53f234
YSPBIG <156817901+YSPBIG@users.noreply.github.com>
2024-01-25 23:35:50 +0000

    NOTE the changes and command

--- count_case.py
+++ count_case.py
@@ -3,11 +3,11 @@ def case_counter():
     upper_case = 0
     lower_case = 0
       
-    user_input = input("Please enter the word here : ")
+    user_input = input("Please enter the word here : ") # To add the word that want the app to run
     for characters in user_input:
-        if characters.isupper():
+        if characters.isupper(): #To add the uppercase since is .isupper is to capture the uppercase alphabet
             upper_case += 1 
-        elif characters.islower():
+        elif characters.islower(): #To add the lowercase since is .islower is to capture the uppercase alphabet
             lower_case += 1
         else:
             pass
