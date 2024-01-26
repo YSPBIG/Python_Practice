a4b072b947d7638003a7cd0df432e4906712be92
YSPBIG <156817901+YSPBIG@users.noreply.github.com>
2024-01-26 00:02:35 +0000

    Add Note of each function

--- morse_code.py
+++ morse_code.py
@@ -30,15 +30,16 @@ def morse_translator(text):
     }
 
 #Convert the text to uppercase to have a applicable for every case sensitivity
-    text = text.upper()
+    text = text.upper() #.upper is to convert the text to be all uppercase and it match with the dictionary
 
-    translated_text = []
+    translated_text = [] #The blank set as the storage of the answer that after translated to morse code
     for char in text:
         if char in morse_code_dict:
-            translated_text.append(morse_code_dict[char])
-        elif char == " ":
-            translated_text.append("/")
-    return " ".join(translated_text) 
+            translated_text.append(morse_code_dict[char]) #If the character is match and in the dictionary in will append(added) into the list 
+        elif char == " ": 
+            translated_text.append("/") # If it is blank so it will be / as the symbol 
+    return " ".join(translated_text)  #.join is to join each result in the set which in this case is translated_text and joined with " "
+    #as the symbol before .join
 
 # Test cases
 print(morse_translator("HELLO WORLD"))
