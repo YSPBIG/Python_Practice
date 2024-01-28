bd2f9b938c332192198e53bbce960f69f721b0da
YSPBIG <156817901+YSPBIG@users.noreply.github.com>
2024-01-28 18:07:05 +0000

    Update count_case.py
    Edited the coding

--- count_case.py
+++ count_case.py
@@ -1,16 +1,21 @@
-def case_counter():
-
-    upper_case = 0
+def case_counter(text):
+#Set the value of each type of words to return the value when counting
+    upper_case = 0 
     lower_case = 0
-      
-    user_input = input("Please enter the word here : ") # To add the word that want the app to run
-    for characters in user_input:
-        if characters.isupper(): #To add the uppercase since is .isupper is to capture the uppercase alphabet
-            upper_case += 1 
-        elif characters.islower(): #To add the lowercase since is .islower is to capture the uppercase alphabet
-            lower_case += 1
-        else:
-            pass
-    print("The number of uppercase is : ",upper_case , "lowercase is : ",lower_case )
+    
+    for character in text: #For all character in the text 
+        if character.isalpha: #If the text is alphabet
+            if character.isupper(): #The character is uppercase
+                upper_case += 1 #The set of uppercase will be added by 1
+            elif character.islower(): #The character is lowercase
+                lower_case += 1 #The set of lowercase will be added by 1
+            else:
+                pass
 
-case_counter()
+    return(f"Uppercase letters : {upper_case}, Lowercase letters: {lower_case}")
+
+# Test cases
+print(case_counter("Hello Wo)rld!"))  # Expected: Uppercase letters: 2, Lowercase letters: 8
+print(case_counter("PYTHON"))  # Expected: Uppercase letters: 6, Lowercase letters: 0
+print(case_counter("python"))  # Expected: Uppercase letters: 0, Lowercase letters: 6
+print(case_counter("1234!@#$)"))  # Expected: Uppercase letters: 0, Lowercase letters: 0
